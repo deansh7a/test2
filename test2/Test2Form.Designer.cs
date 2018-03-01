73294c2d88cf6d01a7ab1b911f39fbd37ff7b0f9
v-deshet <v-deshet@MININT-V6DK8MH>
2018-03-01 14:29:28 -0800

    Added button

--- test2/Test2Form.Designer.cs
+++ test2/Test2Form.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace test2
 {
-    partial class Form1
+    partial class Test2Form
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Test Button";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Test2Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
+            this.Name = "Test2Form";
+            this.Text = "Test2Form";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
